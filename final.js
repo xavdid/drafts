908a56120b556989a2c7c3417869e2b94f3ad51a
David Brownman <beamneocube@gmail.com>
2019-05-29 14:20:57 -0400

    Updates

--- final.js
+++ final.js
@@ -8207,7 +8207,7 @@ const editor = monaco.editor.create(document.getElementById('editor'), {
 editor.getModel().updateOptions({ tabSize: 2 })
 editor.focus()
 
-//////////
+/// ///////
 
 document.getElementById('copy').onclick = () => {
   editor.focus()
